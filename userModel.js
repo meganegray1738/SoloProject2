bc924004d3abd7a01e9966b36306d5418e30a92f
meganegray1738 <156222420+meganegray1738@users.noreply.github.com>
2024-04-30 23:31:00 -0400

    Commit 1

---
+++ userModel.js
@@ -0,0 +1,20 @@
+import * as mongoose from 'mongoose';
+const {Schema, model} = mongoose
+
+const userSchema  = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: false
+    },
+    password: {
+        type: String,
+        required: false
+    }
+})
+
+const UserModel = model('UserModel', userSchema);
+export default UserModel;
---
+++ userRoutes.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import * as userController from '../controllers/userController.js'
+
+const router = express.Router();
+
+//Get all users
+router.get('/', userController.getAllUsers);
+//Get user by ID
+router.get('/getUserByID/:id', userController.getUserByID);
+//Creating a new user
+router.post('/', userController.createUser);
+//Login
+router.post('/login', userController.login);
+export default router;
